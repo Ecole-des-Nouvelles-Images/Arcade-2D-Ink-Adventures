00bcd0ed0f56622443ed2d2f3394b35530856375
Elias <eliaspr2002@gmail.com>
2023-12-01 17:13:08 +0100

    Jour_05_FinishedPlayerColorLogic

--- Chroma/Assets/Elias/Scripts/Data/BaseObject.cs
+++ Chroma/Assets/Elias/Scripts/Data/BaseObject.cs
@@ -1,6 +1,7 @@
 using Elias.Scripts.Managers;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
+using Elias.Scripts.Helper; // Import the helper namespace
 
 namespace Elias.Scripts.Data
 {
@@ -8,37 +9,44 @@ namespace Elias.Scripts.Data
     {
         private Light2D _objectLight;
         private Rigidbody2D _objectRigidbody;
-        private LayerMask _objectLayer;
+        private string _objectLayer; // Change to string to store layer name
         private LayerMask _playerLayer;
-        //private LayerMask _collisionLayer; // New layer for object collisions
         private float _colorTolerance = 3f;
+
+        private Color _currentColor;
 
         void Start()
         {
             _objectRigidbody = GetComponent<Rigidbody2D>();
             _objectLight = GetComponent<Light2D>();
             _playerLayer = LayerMask.NameToLayer("Player");
-            _objectLayer = LayerMask.NameToLayer("Object");
-            //_collisionLayer = LayerMask.GetMask("Object");
-            // Check if the layers are valid
-            if (_playerLayer == -1 || _objectLayer == -1)
+
+            _objectLayer = LayerMask.LayerToName(gameObject.layer); // Store the layer name instead of the index
+
+            if (_playerLayer == -1)
             {
-                Debug.LogError("Player or Object layer not found. Make sure the layers exist in the project settings.");
+                Debug.LogError("Player layer not found. Make sure the layer exists in the project settings.");
             }
+
+            _currentColor = _objectLight.color;
 
             FindObjectOfType<PlayerController>().OnColorChange += HandleColorChange;
         }
 
         private void HandleColorChange(Color newColor)
         {
-            
-            if (GameManager.Instance.AreColorsClose(newColor, this._objectLight.color, _colorTolerance))
+            // Check if the player and object colors are close
+            bool areColorsClose = GameManager.Instance.AreColorsClose(newColor, _objectLight.color, _colorTolerance);
+
+            // Check if the layers should ignore each other based on color
+            bool shouldIgnoreCollision = ColorLayerHelper.ShouldIgnoreCollision(_objectLayer, "Player");
+
+            if (areColorsClose && !shouldIgnoreCollision)
             {
                 if (_objectRigidbody != null)
                 {
-                    // Enable collisions with player and objects
-                    Physics2D.IgnoreLayerCollision(_objectLayer, _playerLayer, false);
-                    //Physics2D.IgnoreLayerCollision(_objectLayer, _collisionLayer, false);
+                    Physics2D.IgnoreLayerCollision(gameObject.layer, _playerLayer, false);
+                    Physics2D.IgnoreLayerCollision(gameObject.layer, gameObject.layer, false);
                     Debug.Log("Je collide !");
                 }
             }
@@ -46,12 +54,13 @@ namespace Elias.Scripts.Data
             {
                 if (_objectRigidbody != null)
                 {
-                    // Disable collisions with player and objects
-                    Physics2D.IgnoreLayerCollision(_objectLayer, _playerLayer, true);
-                    //Physics2D.IgnoreLayerCollision(_objectLayer, _collisionLayer, true);
+                    Physics2D.IgnoreLayerCollision(gameObject.layer, _playerLayer, true);
+                    Physics2D.IgnoreLayerCollision(gameObject.layer, gameObject.layer, true);
                     Debug.Log("Et non !");
                 }
             }
+
+            _currentColor = newColor;
         }
     }
 }
--- Chroma/Assets/Noah/Scripts/Data/ChildObject.cs
+++ Chroma/Assets/Noah/Scripts/Data/ChildObject.cs
@@ -1,9 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using Elias.Scripts.Data;
-using UnityEngine;
 
-public class ChildObject : BaseObject
+namespace Noah.Scripts.Data
 {
+    public class ChildObject : BaseObject
+    {
     
+    }
 }
